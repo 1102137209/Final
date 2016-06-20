4f0508dfbe602ddbfbe5c798f77af638e1161456
SHAN <1102137209@gm.kuas.edu.tw>
2016-06-20 13:51:11 +0800

    OKK

---
+++ Final/Models/DB.cs
@@ -0,0 +1,25 @@
+namespace Final.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class DB : DbContext
+    {
+        public DB()
+            : base("name=DB")
+        {
+        }
+
+        public virtual DbSet<Employees> Employees { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employees>()
+                .HasMany(e => e.Employees1)
+                .WithOptional(e => e.Employees2)
+                .HasForeignKey(e => e.MnangerID);
+        }
+    }
+}
